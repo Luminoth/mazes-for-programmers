e5485e4a28fdcea3b3ff7220d4ce1aa7f7d93d25
Shane <ignignokterr@gmail.com>
2021-11-06 16:26:49 -0700

    Sidewinder impl

--- core/src/algorithms/sidewinder.rs
+++ core/src/algorithms/sidewinder.rs
@@ -2,9 +2,46 @@ use crate::grid::Grid;
 
 use super::Algorithm;
 
+use rand::Rng;
+
 #[derive(Debug, Default)]
 pub struct Sidewinder;
 
 impl Algorithm for Sidewinder {
-    fn run(&self, grid: &mut Grid) {}
+    fn run(&self, grid: &mut Grid) {
+        let mut rng = rand::thread_rng();
+
+        let mut links = Vec::default();
+        for row in grid.row_iter() {
+            let mut run = Vec::new();
+
+            for cell in row {
+                let cell_handle = cell.handle();
+
+                run.push(cell_handle);
+
+                let at_eastern_boundary = cell.east.is_none();
+                let at_northern_boundary = cell.north.is_none();
+
+                let should_close_out =
+                    at_eastern_boundary || (!at_northern_boundary && rng.gen_range(0..=1) == 0);
+
+                if should_close_out {
+                    let index = rng.gen_range(0..run.len());
+                    let member_handle = run[index];
+                    let member = grid.get(member_handle.row, member_handle.col).unwrap();
+                    if let Some(north) = member.north {
+                        links.push((member_handle, north));
+                    }
+                    run.clear();
+                } else {
+                    links.push((cell_handle, cell.east.unwrap()));
+                }
+            }
+        }
+
+        for link in links {
+            grid.link_cells(link.0, link.1);
+        }
+    }
 }
