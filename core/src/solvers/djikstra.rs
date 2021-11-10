22a26c6093fb75d7751b3cc3e965c0ab69e17442
Shane <ignignokterr@gmail.com>
2021-11-09 18:30:39 -0800

    No legit reason to keep this around

--- core/src/solvers/djikstra.rs
+++ core/src/solvers/djikstra.rs
@@ -13,7 +13,7 @@ pub struct Djikstra {
     grid: Grid,
     root: CellHandle,
 
-    distances: RefCell<Option<Distances>>,
+    //distances: RefCell<Option<Distances>>,
     path: RefCell<Option<Distances>>,
 }
 
@@ -22,7 +22,7 @@ impl Djikstra {
         Self {
             grid,
             root: CellHandle::new(root_row, root_column),
-            distances: RefCell::new(None),
+            //distances: RefCell::new(None),
             path: RefCell::new(None),
         }
     }
@@ -36,33 +36,28 @@ impl Djikstra {
         }
     }
 
-    fn path_to(&self, goal: CellHandle) -> Distances {
-        let distances = self.distances.borrow();
-        {
-            let distances = distances.as_ref().unwrap();
+    fn path_to(&self, goal: CellHandle, distances: &Distances) -> Distances {
+        let mut current = goal;
+        let mut current_distance = distances.get_distance(&current).unwrap_or_default();
 
-            let mut current = goal;
-            let mut current_distance = distances.get_distance(&current).unwrap_or_default();
+        let mut breadcrumbs = Distances::new(self.root);
+        breadcrumbs.set_distance(current, current_distance);
 
-            let mut breadcrumbs = Distances::new(self.root);
-            breadcrumbs.set_distance(current, current_distance);
+        while current != self.root {
+            let cell = self.grid.get(current.row, current.col).unwrap();
 
-            while current != self.root {
-                let cell = self.grid.get(current.row, current.col).unwrap();
+            for neighbor in cell.links() {
+                let neighbor_distance = distances.get_distance(neighbor).unwrap_or_default();
+                if neighbor_distance < current_distance {
+                    breadcrumbs.set_distance(*neighbor, neighbor_distance);
 
-                for neighbor in cell.links() {
-                    let neighbor_distance = distances.get_distance(neighbor).unwrap_or_default();
-                    if neighbor_distance < current_distance {
-                        breadcrumbs.set_distance(*neighbor, neighbor_distance);
-
-                        current = *neighbor;
-                        current_distance = distances.get_distance(&current).unwrap_or_default();
-                    }
+                    current = *neighbor;
+                    current_distance = distances.get_distance(&current).unwrap_or_default();
                 }
             }
-
-            breadcrumbs
         }
+
+        breadcrumbs
     }
 }
 
@@ -76,16 +71,18 @@ impl Solver for Djikstra {
 
         if let Some(path) = &*self.path.borrow() {
             Djikstra::cell_contents_from_distances(path, cell)
-        } else if let Some(distances) = &*self.distances.borrow() {
-            Djikstra::cell_contents_from_distances(distances, cell)
+        /*} else if let Some(distances) = &*self.distances.borrow() {
+        Djikstra::cell_contents_from_distances(distances, cell)*/
         } else {
             String::from(" ")
         }
     }
 
     fn solve(&self, goal_row: usize, goal_col: usize) {
-        *self.distances.borrow_mut() = Some(distances(&self.grid, self.root));
-        *self.path.borrow_mut() = Some(self.path_to(CellHandle::new(goal_row, goal_col)));
+        let distances = distances(&self.grid, self.root);
+        *self.path.borrow_mut() =
+            Some(self.path_to(CellHandle::new(goal_row, goal_col), &distances));
+        //*self.distances.borrow_mut() = Some(distances);
     }
 
     fn render_ascii(&self) {
