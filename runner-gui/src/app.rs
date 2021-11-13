06f5ecf03b06f7783b34583ecbbd181614857145
Shane <ignignokterr@gmail.com>
2021-11-13 12:57:20 -0800

    Cleanup

--- runner-gui/src/app.rs
+++ runner-gui/src/app.rs
@@ -1,11 +1,12 @@
 use std::fmt;
+use std::path::Path;
 use std::time::Instant;
 
 use derivative::Derivative;
 use derive_more::Display;
 use eframe::{egui, epi};
 use strum::{EnumIter, IntoEnumIterator};
-use tracing::{debug, error, info};
+use tracing::{debug, info};
 
 use mazecore::generators::*;
 use mazecore::solvers::*;
@@ -70,6 +71,8 @@ pub struct RunnerApp {
 
     generator_type: GeneratorType,
     solver_type: SolverType,
+
+    maze: Option<Grid>,
 }
 
 impl RunnerApp {
@@ -80,7 +83,7 @@ impl RunnerApp {
         ui.selectable_value(selection, generator_type, format!("{}", generator_type));
     }
 
-    fn add_generators(&mut self, ui: &mut egui::Ui) {
+    fn add_generators_select(&mut self, ui: &mut egui::Ui) {
         egui::ComboBox::from_label("Maze Generator")
             .selected_text(format!("{}", self.generator_type))
             .width(130.0)
@@ -91,7 +94,7 @@ impl RunnerApp {
             });
     }
 
-    fn add_solvers(&mut self, ui: &mut egui::Ui) {
+    fn add_solvers_select(&mut self, ui: &mut egui::Ui) {
         egui::ComboBox::from_label("Maze Solver")
             .selected_text(format!("{}", self.solver_type))
             .width(130.0)
@@ -100,6 +103,69 @@ impl RunnerApp {
                     RunnerApp::add_selection(solver_type, ui, &mut self.solver_type);
                 }
             });
+    }
+
+    fn add_generate_button(&mut self, ui: &mut egui::Ui) {
+        // TODO: make this async / threaded and disable the button while generating
+
+        if ui.button("Generate Maze").clicked() {
+            // TODO: this should generate to a grid that we render
+            info!("Processing {}x{} maze ...", self.width, self.height);
+
+            let generator = self.generator_type.generator();
+            let grid = {
+                info!("Running maze generator {} ...", self.generator_type);
+
+                let now = Instant::now();
+                let grid = generator.generate(self.height, self.width);
+                info!("{}ms", now.elapsed().as_secs_f64() * 1000.0);
+
+                grid
+            };
+            debug!("{:?}", grid);
+
+            let (root, goal) = {
+                info!("Finding longest path ...");
+
+                let now = Instant::now();
+                let (root, goal) = grid.longest_path();
+                info!("{}ms", now.elapsed().as_secs_f64() * 1000.0);
+
+                (root, goal)
+            };
+
+            if self.solver_type != SolverType::None {
+                let solver = self.solver_type.solver(grid, root.0, root.1).unwrap();
+
+                {
+                    info!(
+                        "Running solver {} from {:?} to {:?} ...",
+                        self.solver_type, root, goal
+                    );
+
+                    let now = Instant::now();
+                    solver.solve(goal.0, goal.1);
+                    info!("{}ms", now.elapsed().as_secs_f64() * 1000.0);
+                }
+
+                render(&*solver);
+
+                self.maze = Some(solver.grid().clone());
+            } else {
+                render(&grid);
+
+                self.maze = Some(grid);
+            }
+        }
+    }
+
+    fn add_save_button(&mut self, ui: &mut egui::Ui) {
+        if ui.button("Save Maze").clicked() {
+            /*let filename = ...;
+            if let Err(err) = save(&self.???, filename) {
+                error!("{}", err);
+            }*/
+        }
     }
 }
 
@@ -110,84 +176,35 @@ impl epi::App for RunnerApp {
 
     fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.add_generators(ui);
-            self.add_solvers(ui);
+            self.add_generators_select(ui);
+            self.add_solvers_select(ui);
 
             ui.add(egui::Slider::new(&mut self.width, 1..=100).text("Width"));
             ui.add(egui::Slider::new(&mut self.height, 1..=100).text("Height"));
 
             // TODO: the maze render should go here
 
-            // TODO: disable while generating
-            // TODO: do the generation async and render it over time
-            if ui.button("Generate Maze").clicked() {
-                // TODO: this should generate to a grid that we render
-                info!("Processing {}x{} maze ...", self.width, self.height);
-
-                let generator = self.generator_type.generator();
-                let grid = {
-                    info!("Running maze generator {} ...", self.generator_type);
-
-                    let now = Instant::now();
-                    let grid = generator.generate(self.height, self.width);
-                    info!("{}ms", now.elapsed().as_secs_f64() * 1000.0);
-
-                    grid
-                };
-                debug!("{:?}", grid);
-
-                let (root, goal) = {
-                    info!("Finding longest path ...");
-
-                    let now = Instant::now();
-                    let (root, goal) = grid.longest_path();
-                    info!("{}ms", now.elapsed().as_secs_f64() * 1000.0);
-
-                    (root, goal)
-                };
-
-                if self.solver_type != SolverType::None {
-                    let solver = self.solver_type.solver(grid, root.0, root.1).unwrap();
-
-                    {
-                        info!(
-                            "Running solver {} from {:?} to {:?} ...",
-                            self.solver_type, root, goal
-                        );
-
-                        let now = Instant::now();
-                        solver.solve(goal.0, goal.1);
-                        info!("{}ms", now.elapsed().as_secs_f64() * 1000.0);
-                    }
-
-                    if let Err(err) = render(&*solver) {
-                        //, &options.filename) {
-                        error!("{}", err);
-                    }
-                } else {
-                    if let Err(err) = render(&grid) {
-                        //, &options.filename) {
-                        error!("{}", err);
-                    }
-                }
-            }
+            ui.horizontal(|ui| {
+                self.add_generate_button(ui);
+                self.add_save_button(ui);
+            });
         });
     }
 }
 
-fn render(
-    renderable: &(impl Renderable + ?Sized),
-    //filename: &Option<impl AsRef<Path>>,
-) -> anyhow::Result<()> {
+fn render(renderable: &(impl Renderable + ?Sized)) {
     println!();
     renderable.render_ascii();
     println!();
+}
 
-    /*if let Some(filename) = filename {
-        info!("Saving to {:?} ...", filename.as_ref());
+fn _save(
+    renderable: &(impl Renderable + ?Sized),
+    filename: impl AsRef<Path>,
+) -> anyhow::Result<()> {
+    info!("Saving to {:?} ...", filename.as_ref());
 
-        renderable.save_png(filename.as_ref(), 50)?;
-    }*/
+    renderable.save_png(filename.as_ref(), 50)?;
 
     Ok(())
 }
